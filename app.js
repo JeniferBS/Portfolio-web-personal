9db44e8130edb516b958ee5529830bdfc611e071
Javier Aguirre <contacto@javaguirre.net>
2018-06-01 17:27:45 +0200

    Images moved to public, added express.static directory

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ var app = express()
 var exp_handlebars  = require('express-handlebars');
 var hbs = exp_handlebars.create({defaultLayout: 'base'});
 
+CONTACT_EMAIL = 'tuemail';
+
 app.engine(hbs.extname, hbs.engine);
 app.set('view engine', hbs.extname);
+app.use(express.static('public'));
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.render('index');
-  
-
+  // email = process.env.CONTACT_EMAIL;
+  res.render('index', {email: CONTACT_EMAIL});
 });
 
 PORT = process.env.PORT || 3000;

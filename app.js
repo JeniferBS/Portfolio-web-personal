476531f6f763e30468950c8505b503407589700a
Jenifer <jebasa1820@hotmail.es>
2018-06-05 16:42:17 +0200

    scroll

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ var app = express()
 var exp_handlebars  = require('express-handlebars');
 var hbs = exp_handlebars.create({defaultLayout: 'base'});
 
+var context = {
+  title: "My First Blog Post!",
+  author: {
+    id: 47,
+    name: "Yehuda Katz"
+  },
+  body: "My first post. Wheeeee!"
+};
+
+
 app.engine(hbs.extname, hbs.engine);
 app.set('view engine', hbs.extname);
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.render('index');
+ 
   
 
 });
